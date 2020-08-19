84b4c9080f33cc08638735a31f265893f28d7be4
febryardiansyah <febrymuhammad80@gmail.com>
2020-08-19 20:07:53 +0700

    fix api

--- lib/constants/base_url.dart
+++ lib/constants/base_url.dart
@@ -1,5 +1,5 @@
 
-const String BaseUrl = 'https://10.0.2.2:8000/api/';
+const String BaseUrl = 'https://api.illyasviel.pw/api/';
 
 ///TODO:
 // you have to change the API Base url by deploying https://github.com/febryardiansyah/manga-api
--- lib/repositories/recommended_repo.dart
+++ lib/repositories/recommended_repo.dart
@@ -6,7 +6,7 @@ import 'package:mangamint/service/api_service.dart';
 
 class RecommendedRepo{
   Future<List<RecommendedList>>getRecomended()async{
-    final response = await ApiService.api.get(BaseUrl+'recomended');
+    final response = await ApiService.api.get(BaseUrl+'recommended');
     if(response.statusCode == 200){
       var res = json.decode(response.body);
       List<RecommendedList>recommendedList = RecommendedModel.fromJson(res).recommendedList;
